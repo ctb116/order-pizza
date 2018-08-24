f3424fac93ddd93a3c89496d1d6f56dc8faab0b9
Catherine Bradley <catherinetybradley@gmail.com>
2018-08-24 09:54:11 -0700

    add console log feedback for order size and toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,24 @@
 // Business End
-
-
+function Pizza(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+}
 
 // User Interface
 $(document).ready(function() {
 
   $("form#orderPizza").submit(function(event) {
     event.preventDefault();
-    $("input:checkbox[name=orderToppings]:checked").each(function(){
-      var toppingsArry = $(this).val();
-      console.log(toppingsArry);
-    });
 
+    var orderSize = $("#orderSize").val();
+    console.log(orderSize);
+
+    $("input:checkbox[name=orderToppings]:checked").each(function(){
+      var orderToppings = $(this).val();
+
+      var order = new Pizza(orderSize, orderToppings);
+
+                console.log(order.toppings)
+    });
   });
 });
