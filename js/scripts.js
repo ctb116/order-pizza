f7eaf90fa6720d58c2dc4fe8884652a09e593d15
Catherine Bradley <catherinetybradley@gmail.com>
2018-08-24 15:06:26 -0700

    add prototype to add cost based on number of toppings selected

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,30 @@ function Pizza(size, toppings) {
 }
 
 Pizza.prototype.cost = function() {
-  if (this.size === "small" && this.toppings[0] === "cheese") {
-    $("#orderResult").text("$5 for small");
+  if (this.size === "small") {
+    $("#orderItems").text("$5 for a small pizza");
   } else if (this.size === "medium") {
-    $("#orderResult").text("$10 for medium");
+    $("#orderItems").text("$10 for medium pizza");
   } else {
-    $("#orderResult").text("$20 for large");
+    $("#orderItems").text("$20 for large pizza");
   }
 }
-//
-// function Call() {
-//   this.topping
-// }
+
+Pizza.prototype.list = function() {
+  var toppingsAmount = this.toppings;
+  console.log(toppingsAmount.length);
+  if (toppingsAmount.length === 0) {
+    $("#orderToppings").text(" and $0 added for no toppings")
+  } else if (toppingsAmount.length === 1) {
+    $("#orderToppings").text(" and $2 added for 1 topping")
+  } else if (toppingsAmount.length === 2) {
+    $("#orderToppings").text(" and $4 added for 2 toppings")
+  } else if (toppingsAmount.length === 3) {
+    $("#orderToppings").text(" and $6 added for 3 toppings")
+  } else if (toppingsAmount.length === 4) {
+    $("#orderToppings").text(" and $8 added for 4 toppings")
+  }
+}
 
 // User Interface
 $(document).ready(function() {
@@ -38,8 +50,8 @@ $(document).ready(function() {
       toppingsArry.push(orderToppings);
     });
     order.cost();
+    order.list();
     console.log(order.size);
     console.log(order.toppings);
-
   });
 });
