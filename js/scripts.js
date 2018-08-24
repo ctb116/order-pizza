136b10f1b6a9ab96b3ad2b2e53aed1a8c7b33888
Catherine Bradley <catherinetybradley@gmail.com>
2018-08-24 15:47:21 -0700

    add refactoring to toppings price prototype

--- js/scripts.js
+++ js/scripts.js
@@ -16,20 +16,37 @@ Pizza.prototype.cost = function() {
   $("#orderItems").text(sizePrice + " for a " + this.size + " pizza")
 }
 
+// Pizza.prototype.list = function() {
+//   var toppingsAmount = this.toppings;
+//   console.log(toppingsAmount.length);
+//   if (toppingsAmount.length === 0) {
+//     $("#orderToppings").text(" and $0 added for no toppings")
+//   } else if (toppingsAmount.length === 1) {
+//     $("#orderToppings").text(" and $2 added for 1 topping")
+//   } else if (toppingsAmount.length === 2) {
+//     $("#orderToppings").text(" and $4 added for 2 toppings")
+//   } else if (toppingsAmount.length === 3) {
+//     $("#orderToppings").text(" and $6 added for 3 toppings")
+//   } else if (toppingsAmount.length === 4) {
+//     $("#orderToppings").text(" and $8 added for 4 toppings")
+//   }
+// }
+
 Pizza.prototype.list = function() {
   var toppingsAmount = this.toppings;
-  console.log(toppingsAmount.length);
+  var toppingsPrice;
   if (toppingsAmount.length === 0) {
-    $("#orderToppings").text(" and $0 added for no toppings")
+    toppingsPrice = 0;
   } else if (toppingsAmount.length === 1) {
-    $("#orderToppings").text(" and $2 added for 1 topping")
+    toppingsPrice = 2;
   } else if (toppingsAmount.length === 2) {
-    $("#orderToppings").text(" and $4 added for 2 toppings")
+    toppingsPrice = 4;
   } else if (toppingsAmount.length === 3) {
-    $("#orderToppings").text(" and $6 added for 3 toppings")
+    toppingsPrice = 6;
   } else if (toppingsAmount.length === 4) {
-    $("#orderToppings").text(" and $8 added for 4 toppings")
+    toppingsPrice = 8;
   }
+  $("#orderToppings").text(" and $" + toppingsPrice + " added for " + toppingsAmount.length + " toppings")
 }
 
 // User Interface
